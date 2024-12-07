5f162b633fbe79628e6149093a71498e7b0ef5f0
jackiexiao <707610215@qq.com>
2024-12-07 22:14:57 +0800

    添加一个浮动的 tabbar

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,31 +1,31 @@
 const setting = require('./setting/setting.js');
 
 App({
-	onLaunch: function (options) {
+  onLaunch: function (options) {
+    if (!wx.cloud) {
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+    } else {
+      wx.cloud.init({
+        env: setting.CLOUD_ID,
+        traceUser: true,
+      })
+    }
 
-		if (!wx.cloud) {
-			console.error('请使用 2.2.3 或以上的基础库以使用云能力')
-		} else {
-			wx.cloud.init({
-				env: setting.CLOUD_ID,
-				traceUser: true,
-			})
-		}
-
-		this.globalData = {};
-		wx.getSystemInfo({
-			success: e => {
-					this.globalData.statusBarHeight = e.statusBarHeight;
-				let capsule = wx.getMenuButtonBoundingClientRect();
-				if (capsule) { 
-					this.globalData.customBarHeight = capsule.bottom + capsule.top - e.statusBarHeight;
-					this.globalData.capsule = capsule;
-				} else {
-					this.globalData.customBarHeight = e.statusBarHeight + 50;
-				
-				} 
-			}
-		});
-	}, 
-	 
+    this.globalData = {
+      selectedTabIndex: 0
+    };
+    
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.statusBarHeight = e.statusBarHeight;
+        let capsule = wx.getMenuButtonBoundingClientRect();
+        if (capsule) { 
+          this.globalData.customBarHeight = capsule.bottom + capsule.top - e.statusBarHeight;
+          this.globalData.capsule = capsule;
+        } else {
+          this.globalData.customBarHeight = e.statusBarHeight + 50;
+        } 
+      }
+    });
+  }, 
 })
